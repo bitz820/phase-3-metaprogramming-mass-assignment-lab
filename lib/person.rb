c320509fca98a99bd770606fecf6347d1737ad3d
Mark <mark.n.bacon.jr@gmail.com>
2022-04-28 16:27:54 -0400

    commit tree 2d4b173ec8977fd1f12f92ad3506675696de9d20

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,18 @@
+require "pry"
+
 class Person
+
+  # attr_accessor :name, :birthday, :hair_color, :eye_color, :height,
+  # :weight, :handed, :complexion, :t_shirt_size,
+  # :wrist_size, :glove_size, :pant_length, :pant_width
+
+  def initialize attributes
+    if attributes
+    attributes.each do |key, value|
+      self.class.attr_accessor(key)
+      self.send("#{key}=", value)
+    end
+    end
+  end
   # your code here
 end
